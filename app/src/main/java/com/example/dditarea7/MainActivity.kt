b81fbc96a65ef5838a9a27c73c933c2c9cf77f6f
Diego Hernández Rubiano <diego.herrub@educa.jcyl.es>
2024-12-27 21:44:31 +0100

    wip - popup menu configurado

--- app/src/main/java/com/example/dditarea7/MainActivity.kt
+++ app/src/main/java/com/example/dditarea7/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.dditarea7
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.MenuInflater
 import android.view.View
+import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dditarea7.data.local.LocalMockImages
 import com.example.dditarea7.app.findCenterView
@@ -16,6 +24,53 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Configurate menu more
+        val buttonMore = findViewById<ImageButton>(R.id.button_more)
+        buttonMore.setOnClickListener { view ->
+
+            // Apply the custom style to the popup menu
+            // val contextThemeWrapper = ContextThemeWrapper(this, R.style.style_red_item_menu_more)
+            // val popupMenu = PopupMenu(contextThemeWrapper, view)
+
+            // Create the popup menu
+            val popupMenu = PopupMenu(this, view)
+            val inflater: MenuInflater = popupMenu.menuInflater
+            inflater.inflate(R.menu.menu_more_options, popupMenu.menu)
+
+            // Apply the custom style to the popup menu item "Report"
+            val menuItemReport = popupMenu.menu.findItem(R.id.action_report)
+            val spannableTitle = SpannableString(menuItemReport.title)
+            spannableTitle.setSpan(
+                ForegroundColorSpan(getColor(R.color.md_theme_error)), // Set the text color
+                0,
+                spannableTitle.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            menuItemReport.title = spannableTitle
+
+            // Handle the click selected option
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.action_report -> {
+                        // Handle settings option click
+                        handleReportAction()
+                        true
+                    }
+
+                    R.id.action_share -> {
+                        // Handle share option click
+                        handleShareAction()
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+            popupMenu.show()
+        }
+
 
         // Instancio las imágenes
         var localMockImages = LocalMockImages().get()
@@ -40,6 +95,8 @@ class MainActivity : AppCompatActivity() {
                 updateIndicators(position)
             }
         })
+
+
     }
 
     private fun setupIndicators(count: Int) {
@@ -69,6 +126,17 @@ class MainActivity : AppCompatActivity() {
             }
             indicator.setBackgroundResource(backgroundRes)
         }
+    }
+
+    // functions for the menu more
+    private fun handleReportAction() {
+        // Handle report action
+        Toast.makeText(this, "Option Report clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun handleShareAction() {
+        // Handle share action
+        Toast.makeText(this, "Option Share clicked", Toast.LENGTH_SHORT).show()
     }
 
 }
